69ab6d60f2395ed6c2a7c1d31cf349da5583676b
L8ton-crypto <L8ton-crypto@users.noreply.github.com>
2026-03-08 14:28:10 +0000

    Add image domain whitelist for Shopify CDN and retailer images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.cgarsltd.co.uk',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.smoke-king.co.uk',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
